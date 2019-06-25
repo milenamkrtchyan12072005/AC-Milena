1a761b2656b007c1b76ec9c95ab124f56c761c60
milenamkrtchyan12072005 <milenamkrtchyan12072005>
2019-06-25 16:12:21 +0400

    new examples

--- example2/sketch.js
+++ example2/sketch.js
@@ -1,23 +1,49 @@
+var mic;
+var button;
+var smoothMicLevel=0;
 var t;
+var song;
+var button;
+var amplitude;
+
+function preload() {
+  song = loadSound("../audio/myBeat.mp3");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  t = 0;
+	createCanvas(windowWidth, windowHeight-100);
+    background(0);
+	button = createButton("Listen");
+    button.mousePressed(toggleListen);
+	mic = new p5.AudioIn()
+    mic.start();
+    t = 0;
 }
 
 function draw() {
   background(0, 5);
-
+  micLevel = mic.getLevel();
   var x = width * noise(t);
   var y = height * noise(t+5);
   var r = 255 * noise(t+10);
   var g = 255 * noise(t+15);
   var b = 255 * noise(t+20);
-  
+  smoothMicLevel = lerp(smoothMicLevel, micLevel,0.2);
   noStroke();
   fill(r, g, b);
   ellipse(x, y, 120, 120);
 
   t = t + 0.005;
+}
+
+function toggleListen() {
+	if (getAudioContext().state !== 'running') {
+    	getAudioContext().resume();
+		text('listening to audio', width/2, height/2);
+		button.html("Stop");
+	} else {
+        text('click Play button to start', width/2, height/2);
+
+        button.html("Listen");
+    }
 }
--- example3/sketch.js
+++ example3/sketch.js
@@ -1,30 +1,37 @@
-var t;
+var song;
+var button;
+var amplitude;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  stroke(100, 255);
-  noFill();
-  t = 0;
+function preload() {
+  song = loadSound("../audio/myBeat.mp3");
 }
 
-function draw() {
-  translate(width/2, height/2);
-  beginShape();
-  for (var i = 0; i < 200; i++) {
-    var ang = map(i, 0, 200, 0, TWO_PI);
-    var rad = 200 * noise(i * 0.01, t * 0.005);
-    var x = rad * cos(ang);
-    var y = rad * sin(ang);
-    curveVertex(x, y);
-  }
-  endShape(CLOSE);
+function setup() {
+    createCanvas(windowWidth, windowHeight-100);
+    background(51);
+    slider = createSlider(0,1,0.5,0.01);
+    button = createButton("Play");
+    button.mousePressed(togglePlay);
+	// song.loop();
+    amplitude = new p5.Amplitude();
+}
 
-  t += 1;
+function togglePlay() {
+    if (!song.isPlaying()){
+        song.loop();
+        button.html("Pause");
+    } else {
+        song.stop();
+        button.html("Play");
+    }
+}
 
-  // clear the background every 600 frames using mod (%) operator
-  if (frameCount % 600 == 0) {
-	background(255);
-  }
+function draw(){
+    background(0);
+    song.setVolume(slider.value());
 
+    var vol = amplitude.getLevel();
+    var diam = map(vol, 0, 1, 10, height);
+    fill(255, 0, 255,200);
+    ellipse(width/2, height/2, diam, diam);
 }
